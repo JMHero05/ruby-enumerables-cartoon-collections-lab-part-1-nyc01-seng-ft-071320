3087a7180832baaf7ee1a29e3a78bffec9536829
Jake Mills <jake.mills.05@gmail.com>
2020-07-05 18:20:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |dwarf, index|
-    p array[index] = "#{index}. #{dwarf}"
+    p array[index] = "#{index + 1}. #{dwarf}"
   end
 end
